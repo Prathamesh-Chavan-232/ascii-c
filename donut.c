67f058e701cc39d942de3cf43130cbf8857bd83a
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2023-02-08 13:20:10 +0530

    Creating donut.c

---
+++ donut.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char **argv) {
+    float A = 0, B = 0, i, j, z[1760];
+    char b[1760];
+    printf("\n1b2j");
+    for (;;) {
+        memset(b, 32, 1760);
+        memset(z, 0, 7040);
+        for (j = 0; 6.28 > j; j += 0.01) {
+            for (i = 0; 6.28 > i; i += 0.08) {
+                float c = sin(i), d = cos(j), e = sin(A), f = sin(j), g = cos(A),
+                      h = d + 2, D = 1 / (c * h * e + f * g + 5), l = cos(i),
+                      m = cos(B), n = sin(B),
+                      t = c * h * g - f * c;
+            }
+        }
+    }
+    return 0;
+}
